7ae23811c23afde8941f612a9d34debf451cfff1
Ricardo Carvalho <rickmanu@gmail.com>
2016-03-07 17:02:28 -0300

    First commit

---
+++ wp-content/themes/zine/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Zine
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'zine' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'zine' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'zine' ), 'zine', '<a href="http://galdar.com.br/" rel="designer">Ricardo Carvalho</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/zine/gulpfile.js
@@ -0,0 +1,79 @@
+'use strict';
+
+var gulp 		= require('gulp'),
+	sass 		= require('gulp-sass'),
+	jshint 		= require('gulp-jshint'),
+	concat 		= require('gulp-concat'),
+	uglify 		= require('gulp-uglify'),
+	imagemin 	= require('gulp-imagemin'),
+	plumber 	= require('gulp-plumber'),
+	notify 		= require('gulp-notify'),
+	livereload 	= require('gulp-livereload'),
+	deploy 		= require('gulp-deploy-git');
+
+gulp.task('default', function () {
+	console.log('default gulp task...')
+});
+
+gulp.task('sass', function () {
+	return gulp.src('./assets/css/sass/**/*.scss')
+			.pipe(plumber(plumberErrorHandler))
+			.pipe(sass({outputStyle: 'compressed'}))
+			.pipe(gulp.dest('./assets/css'))
+			.pipe(livereload());
+});
+
+// gulp.task('sass:watch', function () {
+// gulp.watch('./assets/css/sass/**/*.scss', ['sass']);
+// });
+
+gulp.task('js', function () {
+	gulp.src('./assets/js/src/*.js')
+			.pipe(plumber(plumberErrorHandler))
+			.pipe(jshint())
+			.pipe(jshint.reporter('default'))
+			.pipe(concat('theme.js'))
+			.pipe(uglify())
+			.pipe(gulp.dest('./assets/js/dist/'))
+			.pipe(livereload());
+});
+
+// Uglify with gulp-uglify
+gulp.task('compress', function () {
+	return gulp.src('./assets/js/dist/*.js')
+			.pipe(uglify())
+			.pipe(gulp.dest('./assets/js/dist/'));
+});
+
+gulp.task('img', function() {
+	gulp.src('./assets/img/*.{png,jpg,gif}')
+		.pipe(plumber(plumberErrorHandler))
+		.pipe(imagemin({
+			optimizationLevel: 7,
+			progressive: true
+		}))
+		.pipe(gulp.dest('./assets/img/'))
+		.pipe(livereload());
+});
+
+gulp.task('watch', function() {
+	livereload.listen();
+	gulp.watch('./assets/css/sass/**/*.scss', ['sass']);
+	gulp.watch('./assets/js/src/*.js', ['js']);
+	gulp.watch('./assets/img/*.{png,jpg,gif}', ['img']);
+});
+
+var plumberErrorHandler = { errorHandler: notify.onError({
+		title: 'Gulp',
+		message: 'Error: <%= error.message %>'
+	})
+};
+
+gulp.task('deploy', function() {
+	return gulp.src('../../../../')
+		.pipe(deploy({
+			repository: 'https://github.com/Darciro/Zine.git'
+		}));
+});
+
+gulp.task('default', ['sass', 'js', 'img', 'watch']);
