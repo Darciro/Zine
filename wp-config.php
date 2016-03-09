b8041144c20c82feb02f4c572a39fc41eb8d1231
darciro <rickmanu@gmail.com>
2016-03-09 11:05:24 -0700

    Remocao do arquivo de configuracao do versionamento

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'zine_db');
+define('DB_NAME', 'galdar_cliente_zine_db');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'darcirodba');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'G!+W{R-$$U*r');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -36,6 +36,10 @@ define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
+
+define('WP_SITEURL','http://galdar.com.br/espaco-do-cliente/zine');
+define('WP_HOME','http://galdar.com.br/espaco-do-cliente/zine');
+define('RELOCATE',true);
 
 /**#@+
  * Authentication Unique Keys and Salts.
