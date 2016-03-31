ac256744cef5863837f99387daf5f597c3e40d95
Ricardo Carvalho <rickmanu@gmail.com>
2016-03-31 13:53:38 -0300

    ACF e Formatos de post (Vídeo e Citação)

--- wp-content/themes/zine/template-parts/content.php
+++ wp-content/themes/zine/template-parts/content.php
@@ -10,6 +10,22 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('zine-posts'); ?>>
+	<?php if ( has_post_format( 'video' )) { ?>
+	<div class="entry-video-wrapper">
+		<?php the_field('link_do_video'); ?>
+	</div>
+	<?php } ?>
+
+	<?php if ( has_post_format( 'quote' )) { ?>
+		<div class="entry-quote">
+			<blockquote>
+				<h2 class="entry-title entry-title--quoted"><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_field('citacao_texto'); ?></a></h2>
+				<footer>— <?php the_field('citacao_autor'); ?></footer>
+			</blockquote>
+		</div>
+	<?php } ?>
+
+	<?php if ( !has_post_format() ) { ?>
 	<div class="entry-thumb">
 		<?php if ( has_post_thumbnail() ) { ?>
 			<a href="<?php echo esc_url( get_permalink() ); ?>">
@@ -17,6 +33,8 @@
 			</a>
 		<?php } ?>
 	</div>
+	<?php } ?>
+
 	<header class="entry-header">
 		<?php if ( 'post' === get_post_type() ) : ?>
 			<div class="entry-meta">
@@ -24,13 +42,16 @@
 			</div><!-- .entry-meta -->
 		<?php endif; ?>
 
-		<?php if ( is_single() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			else :
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		<?php if ( !has_post_format( 'quote' )):
+			if ( is_single() ) :
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				else :
+					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			endif;
 		endif; ?>
 	</header><!-- .entry-header -->
 
+	<?php if ( !has_post_format( 'quote' )): ?>
 	<div class="entry-content">
 		<?php
 			if( is_home() ):
@@ -52,6 +73,7 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
+	<?php endif; ?>
 
 	<?php if( !is_single() ): ?>
 	<footer class="entry-footer">
